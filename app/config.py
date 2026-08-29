4791bc68e1339c5ca507be0151bdf673232c3a42
Thanyani <thanyaniinnocent20@gmail.com>
2026-08-29 09:58:23 +0200

    setting the env and requiremts

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    database_url: str
+    
+    model_config = SettingsConfigDict(env_file='.env.example', env_file_encoding='utf-8')
+    
+settings = Settings()
