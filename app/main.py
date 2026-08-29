2a4fcec6d3f24837c470c4fe6cd2bf47185e7de4
Thanyani <thanyaniinnocent20@gmail.com>
2026-08-29 11:10:15 +0200

    created and registered the user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.config import settings
 from app.database.db import Base, engine
+from app.models.user_model import UserTable
 
 app = FastAPI()
 
---
+++ app/models/user_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from app.database.db import Base
+
+class UserTable(Base):
+    __tablename__ = 'Users'
+    user_id = Column(Integer, primary_key=True, index=True)
+    name = Column(name='first_name', type_=String(50), nullable=False)
+    surname = Column(name='last_name', type_=String(50), nullable=False)
+    email = Column(name='email',type_=String(100), nullable=False, unique=True,)
+    hash_password = Column(name='hash_password', type_=String(255), nullable=False)
+    is_verified = Column(name='is_verified', type_=Boolean, default=False)
