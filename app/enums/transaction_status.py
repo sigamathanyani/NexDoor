2ed835c2cc6ee50cbeb5256d6c6cf79a42f59e6c
Thanyani <thanyaniinnocent20@gmail.com>
2026-09-08 19:39:36 +0200

    fixed transaction status

--- app/enums/transaction_status.py
+++ app/enums/transaction_status.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TransactionStatus(Enum):
-    PENDING = 'PENDING'
-    REJETCED = 'REJETCED'
-    IN_PROGRESS = 'IN_PROGRESS'
-    ACCEPTED = 'ACCEPTED'
+    PENDING = 'PENDING' # The customer just created the transaction and waitong fro provider
+    REJETCED = 'REJETCED' # Provider rejected the transaction
+    IN_PROGRESS = 'IN_PROGRESS' # The rental/service is currently happening
+    COMPLETED = 'COMPLETED' # The transaction is finished (SOLD/RENTAL DONE/SERVICE DONE)
