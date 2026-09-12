e664ba1c36412fae964feebd80f564d67b48b58b
Thanyani <thanyaniinnocent20@gmail.com>
2026-09-12 14:16:52 +0200

    sorting functionality implementation

--- app/schemas/filter_schema.py
+++ app/schemas/filter_schema.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 from app.enums.categories import Category
 from app.enums.product_type import ProductType
+from app.enums.sorting import SortBy, SortOrder
 
 class FilterParams(BaseModel):
     model_config = {"extra": "allow"}
@@ -9,3 +10,5 @@ class FilterParams(BaseModel):
     product_query: str | None = None # query_params
     product_type: list[ProductType] | None = None # query_params
     category_filter: list[Category] | None = None # query_params
+    sort_by: SortBy | None = None
+    sort_order: SortOrder | None = None
